f5b2ffcaf3af3cd0ea8f18bf2dea36a32ad2c971
Dinesh <dineshsingh9350@gmail.com>
2020-06-25 10:58:31 +0530

    dark mode updated

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,7 +1,7 @@
 var options = {
   bottom: '64px', // default: '32px'
   right: 'unset', // default: '32px'
-  left: '32px', // default: 'unset'
+  right: '32px', // default: 'unset'
   time: '0.5s', // default: '0.3s'
   mixColor: '#fff', // default: '#fff'
   backgroundColor: '#fff',  // default: '#fff'
