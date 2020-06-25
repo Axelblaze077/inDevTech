a70b1ce8ce21f5770849146fd858308952102c49
Dinesh <dineshsingh9350@gmail.com>
2020-06-25 16:40:23 +0530

    dark mode time reduced

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -2,7 +2,7 @@ var options = {
   bottom: '64px', // default: '32px'
   right: 'unset', // default: '32px'
   right: '32px', // default: 'unset'
-  time: '0.5s', // default: '0.3s'
+  time: '0s', // default: '0.3s'
   mixColor: '#fff', // default: '#fff'
   backgroundColor: '#fff',  // default: '#fff'
   buttonColorDark: '#100f2c',  // default: '#100f2c'
